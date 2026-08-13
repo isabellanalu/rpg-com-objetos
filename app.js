c519294f032523d6108c5263d23a02eda8f27421
isabellanalu <isabellanalurodrigues@gmail.com>
2026-08-13 09:30:31 -0300

    criando os ataques entre os personagens

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
-const guerreiro = {
-    nome: "Thorin",
-    vida: 60,
-    ataque: 20,
-
-    causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
-    },
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
+class personagem{
+    constructor(nome, vida, ataque){
+        this.nome = nome,
+        this.vida = vida,
+        this.ataque = ataque
     }
 
-}
-const mago = {
-    nome: "Gandalfe",
-    vida: 60,
-    ataque: 35,
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-    },
-
-    causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
+    causaDano(personagem){
+        personagem.recebeDano(this.ataque)
+        
     }
-
-}
-const arqueiro = {
-    nome: "Legolas",
-    vida: 80,
-    ataque: 25,
     
-    recebeDano: function(quantidade){
-    this.vida = this.vida - quantidade
-    },
-
-    causaDano: function(personagem){
-    personagem.recebeDano(this.ataque)
+    recebeDano(quantidade){
+       this.vida = this.vida - quantidade 
+        
+       if(this.vida <=0){
+        this.vida = "eliminado"
     }
-
 }
-guerreiro.causaDano(mago)
-console.log(mago.vida)
+}
+const druida = new personagem("Kode", 80, 25)
+
+const guerreiro = new personagem("Thorin", 60, 20)
+
+const mago = new personagem("Gandalfe", 60, 25)
+
+const arqueiro = new personagem("Legolas", 80, 25)
+
+const elfo = new personagem ("Dobby", 95, 15)
+
+const anao = new personagem ("Soneca", 30, 90)
+
+const princesa = new personagem ("Cinderela", 85, 30)
+
+
+console.log (guerreiro, "\n", druida, "\n", mago,"\n", arqueiro,"\n", elfo, "\n", anao, "\n", princesa   )
